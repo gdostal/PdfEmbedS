81eb43962908e0c9a12735396e033e87d01e62f0
Graham Dostal <gdostal@tplibrary.org>
2023-02-24 15:32:22 -0600

    adds PdfFunctions.php to calc pdfs aspect ratio

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,7 @@ return [
     'file_renderers' => [
         'invokables' => [
             'pdf_embed' => 'PdfEmbed\PdfRenderer',
+            'pdf_functions' => 'PdfEmbed\PdfFunctions',
         ],
         'aliases' => [
             'application/pdf' => 'pdf_embed',
--- src/PdfRenderer.php
+++ src/PdfRenderer.php
@@ -6,16 +6,20 @@ use Omeka\Api\Representation\MediaRepresentation;
 use Omeka\Media\FileRenderer\RendererInterface;
 use Zend\View\Renderer\PhpRenderer;
 
+require('PdfFunctions.php');
+
 class PdfRenderer implements RendererInterface
 {
     public function render(PhpRenderer $view, MediaRepresentation $media, array $options = [])
     {
-        $html = '<div class="object-embed" style="--aspect-ratio: 0.75/1;"><object data="%s" width="563" height="750" %s></object></div>';
+        $html = '<div class="object-embed" style="--aspect-ratio: %s;"><object data="%s" %s %s></object></div>';
         $data = $view->escapeHtml( $media->originalUrl() );
+		$ratio = aspect_ratio( $media );
         !empty( $media->mediaType() ) ? $type = 'type="' . $view->escapeHtml( $media->mediaType() ) . '"' : $type = null ;
 
-        $view->headLink()->appendStylesheet($view->assetUrl('css/pdfObjectEmbed.css' , 'PdfObjectEmbed'));
+        $view->headLink()->appendStylesheet($view->assetUrl('css/pdfObjectEmbed.css' , 'PdfEmbed'));
 
-        return sprintf( $html , $data , $type );
+        return sprintf( $html , $ratio['0'], $data , $ratio['1'] , $type );
     }
+    
 }
